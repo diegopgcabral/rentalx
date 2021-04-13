9223211b218943234382ab65be9f50554550089f
Diego Cabral <diegopgc@gmail.com>
2021-04-13 14:24:28 -0300

    criando models/repositories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,28 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidV4(),
-    name,
-    description,
-  };
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  categories.push(category);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists!' });
+  }
+
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const categories = categoriesRepository.list();
+
+  return response.status(201).json(categories);
 });
 
 export { categoriesRoutes };
